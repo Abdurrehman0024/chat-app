08d5715b696cc93ae144d67696c65d067509c877
Abdurrehman0024 <abdullah.atique@groltech.com>
2024-10-09 17:34:54 +0500

    setup user routes

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -27,14 +27,46 @@ export const sendMessage = async (req,res)=>{
       conversation.messages.push(newMessage._id)
     }
 
-    await conversation.save();
-    await newMessage.save();
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json({newMessage})
     
 
-    }catch (error) {
+    }
+    catch (error) {
       console.error("Error in protectRoute:", error);
       res.status(500).json({error:"Internal server error"})
+   }
+};
+
+export const getMessage = async (req,res)=>{
+  try {
+    
+    const { id:userToChatId} = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: {$all:[senderId,userToChatId]}
+    }).populate("messages");
+
+      if(!conversation){
+        return res.status(200).json([])
+      };
+      
+
+      const messages = conversation.messages
+
+      res.status(200).json(messages);
+
+ 
+
+
   }
+
+  
+  catch (error) {
+    console.error("Error in getMessage route:", error);
+      res.status(500).json({error:"Internal server error"})
+  }
+
 };
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -12,8 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // from (req.body)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 app.listen(PORT, ()=> {
