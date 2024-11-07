a1461c5fd6496395abc8225d700159941048df77
Abdurrehman0024 <abd.rehman.0024@gmail.com>
2024-11-07 22:18:40 +0500

    Add .env to .gitignore

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,7 @@ import userRoutes from "./routes/user.route.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +19,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*",(req,res) =>{
+  res.sendFile(path.join(__dirname,"Frontend" ,"dist","index.html"));
+})
 
 connectToMongoDb();
 server.listen(PORT, ()=> {
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  build: {
-    commonjsOptions: {
-        include: ['node_modules/buffer/index.js']
-    }
-},
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy:{
-      '/api':{
-        target:'http://localhost:8000',
-     }
-    }
-  },
-})
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:8000",
+			},
+		},
+	},
+});
